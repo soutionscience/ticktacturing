b0a6be621ca1138de2bbe606a47c19662ee3cb21
Rubin Njagi <marshnjagi@yahoo.com>
2018-05-30 00:25:14 +0300

    before eject

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <h1>Still working</h1>
+    </div>, 
+    document.getElementById('root'));
 registerServiceWorker();
